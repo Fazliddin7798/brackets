13c468e9c3812d6183fa5e175dbb870e01b4aef4
Fazliddin7798 <fazliddin.uralov.98@gmail.com>
2022-02-25 00:42:24 +0600

    feat: create js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+  module.exports = function check(str, bracketsConfig) {
+    let faz = [];
+    let brackets = new Map(bracketsConfig);
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str[i] == brackets.get(faz[faz.length - 1])) {
+            faz.pop();
+        }
+         else {
+            faz.push(str[i])
+        }
+
+    }
+
+
+   return faz.length !== 0 ? false : true;
+
 }
